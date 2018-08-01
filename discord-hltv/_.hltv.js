c9970e3394f88dff8905623987f39ec2b5ce5e9f
Felix <DukeBruno123@gmail.com>
2018-08-01 08:29:59 +0200

    kinda fixed, not really, module is broken

--- discord-hltv/_.hltv.js
+++ discord-hltv/_.hltv.js
@@ -1,4 +1,6 @@
 const { HLTV } = require('hltv');
+const moment = require('moment');
+require('moment-duration-format');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -19,6 +21,10 @@ const enums = {
 	TeamNames: {
 		TERRORIST: 'T',
 		CT: 'CT'
+	},
+	RoundEndNames: {
+		TERRORIST: 'Terrorists',
+		CT: 'Counter-Terrorists'
 	}
 }
 
@@ -38,73 +44,75 @@ client.on('message', async (msg) => {
 		var updateInterval = undefined;
 		var lastEvent = undefined;
 		var lastEventsLength = 0;
+		var roundStart = undefined;
 
 		HLTV.getMatch({id: parseInt(args[0])}).then(async (res) => {
 			if (res.live && res.hasScorebot) {
-				HLTV.connectToScorebot({id: parseInt(args[0]), onConnect: async() => {
+				HLTV.connectToScorebot({id: parseInt(args[0]), onConnect: async () => {
 					await m.edit('Scorebot connected... This message will update itself in a few seconds. Please wait...');
 
-					updateInterval = setInterval(() => {
+					updateInterval = setInterval(async () => {
 						if (events.length <= 0 || events.length > 15) return m.edit('Waiting for next round start...');
 						if (events.length === lastEventsLength) return;
 						lastEventsLength = events.length;
 
 						var backup = [];
 						for (let i = 1; i < events.length; i++) backup.push(events[i]);
-						m.edit(events[0] + '\n```\n' + backup.reverse().join('\n') + '```');
+						await m.edit(events[0] + '\n```\n' + backup.reverse().join('\n') + '```');
+
+						HLTV.getMatch({id: parseInt(args[0])}).then(async (res) => {
+							if (res.winnerTeam) {
+								clearInterval(updateInterval);
+
+								var backup = [];
+								for (let i = 1; i < events.length; i++) backup.push(events[i]);
+								await m.edit(events[0] + '\n```\n' + backup.reverse().join('\n') + '```\n\n```md\n# ' + res.winnerTeam.name + ' has won this match```');
+							}
+						});
 					}, 2000);
 				}, onDisconnect: async () => {
 					clearInterval(updateInterval);
 					await m.edit(m.content + '\n\n**Scorebot disconnected**');
-				}, onScoreboardUpdate: (data) => {
+				}, onScoreboardUpdate: async (data) => {
 					lastScoreboard = data;
-
-					if (false /* Stuff to figure out if match is over or not */) {
-						setTimeout(() => {
-							clearInterval(updateInterval);
-							m.edit('Match over // TODO');
-						}, 10000);
-					}
 				}, onLogUpdate: async (data) => {
 					if (data.log[0].RoundStart) {
 						events = [];
+						roundStart = new Date().getTime();
+
 						if (!lastScoreboard) events.push('Round started\n*Information missing please wait for the next round to start*');
-						else events.push('__Round ' + parseInt(lastScoreboard.currentRound + 1) + ' started__\n**(CT) ' + lastScoreboard.ctTeamName + ' ' + lastScoreboard.ctTeamScore + ':' + lastScoreboard.terroristScore + ' ' + lastScoreboard.terroristTeamName + '(T)**');
+						else events.push('__Round ' + parseInt(lastScoreboard.currentRound + 1) + ' started__\n**(CT) ' + lastScoreboard.ctTeamName + ' ' + lastScoreboard.ctTeamScore + ':' + lastScoreboard.terroristScore + ' ' + lastScoreboard.terroristTeamName + ' (T)**');
 					} else if (data.log[0].BombPlanted && events.length >= 1) {
-						var toPush = data.log[0].BombPlanted.playerName + ' planted the bomb in a (CT) ' + data.log[0].BombPlanted.ctPlayers + ' on ' + data.log[0].BombPlanted.tPlayers + ' (T) scenario';
+						var toPush = data.log[0].BombPlanted.playerNick + ' planted the bomb in a (CT) ' + data.log[0].BombPlanted.ctPlayers + ' on ' + data.log[0].BombPlanted.tPlayers + ' (T) scenario';
 						if (lastEvent === toPush) return;
 						
 						events.push(toPush);
 						lastEvent = toPush;
 					} else if (data.log[0].BombDefused && events.length >= 1) {
-						var toPush = data.log[0].BombDefused.playerName + 'defused the bomb';
+						var toPush = data.log[0].BombDefused.playerNick + ' defused the bomb';
 						if (lastEvent === toPush) return;
 						
 						events.push(toPush);
 						lastEvent = toPush;
 					} else if (data.log[0].Kill && events.length >= 1) {
-						var toPush = '(' + enums.TeamNames[data.log[0].Kill.killerSide] + ') ' + data.log[0].Kill.killerName + ' killed (' + enums.TeamNames[data.log[0].Kill.victimSide] + ') ' + data.log[0].Kill.victimName + (data.log[0].Kill.headShot ? ' (Headshot)' : '');
+						var toPush = '(' + enums.TeamNames[data.log[0].Kill.killerSide] + ') ' + data.log[0].Kill.killerNick + ' killed (' + enums.TeamNames[data.log[0].Kill.victimSide] + ') ' + data.log[0].Kill.victimName + (data.log[0].Kill.headShot ? ' (Headshot)' : '');
 						if (lastEvent === toPush) return;
 						
 						events.push(toPush);
 						lastEvent = toPush;
 					} else if (data.log[0].RoundEnd && events.length >= 1) {
-						var toPush = '\n' + enums.TeamNames[data.log[0].RoundEnd.winner] + ' win\n' + enums.RoundEndReasons[data.log[0].RoundEnd.winType] + '\n\n';
+						var toPush = undefined;
+
+						if (!lastScoreboard) toPush = 'Round over\n*Information missing please wait for the next round to start*';
+						else toPush = '__Round ' + lastScoreboard.currentRound + ' over__\nTook: ' + moment.duration(new Date().getTime() - roundStart).format(' m [mins], s [secs]') + '\n**Winner: ' + enums.RoundEndNames[data.log[0].RoundEnd.winner] + '**\n' + enums.RoundEndReasons[data.log[0].RoundEnd.winType] + '\n**(CT) ' + lastScoreboard.ctTeamName + ' ' + lastScoreboard.ctTeamScore + ':' + lastScoreboard.terroristScore + ' ' + lastScoreboard.terroristTeamName + ' (T)**';
+
 						if (lastEvent === toPush) return;
-						
-						events.push(toPush);
+
 						lastEvent = toPush;
-
-						if (data.log[0].RoundEnd.winner === 'CT') events[0] = '__Round ' + parseInt(lastScoreboard.currentRound + 1) + ' started__\n**`CT ' + parseInt(lastScoreboard.ctTeamScore + 1) + ':' + lastScoreboard.terroristScore + ' T`**';
-						if (data.log[0].RoundEnd.winner === 'TERRORIST') events[0] = '__Round ' + parseInt(lastScoreboard.currentRound + 1) + ' started__\n**`CT ' + lastScoreboard.ctTeamScore + ':' + parseInt(lastScoreboard.terroristScore + 1) + ' T`**';
-
-						if (events.length <= 0 || events.length > 15) return;
-						if (events.length === lastEventsLength) return;
-						lastEventsLength = events.length;
 
 						var backup = [];
 						for (let i = 1; i < events.length; i++) backup.push(events[i]);
-						m.edit(events[0] + '\n```\n' + backup.reverse().join('\n') + '```');
+						m.edit(toPush + '\n```\n' + backup.reverse().join('\n') + '```');
 					}
 				}});
 			} else {
