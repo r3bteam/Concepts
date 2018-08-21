656febcfd6b9f62acb80a1849bd05d77e4dcbb34
Felix <DukeBruno123@gmail.com>
2018-08-21 22:31:30 +0200

    Maybe (?) fixed an issue when the connection cut out for a short time.

--- discord-steamappchanges/_.steamAppChanges.js
+++ discord-steamappchanges/_.steamAppChanges.js
@@ -7,6 +7,9 @@ const client = new Discord.Client();
 
 const config = require('./config.json');
 
+var ws = undefined
+var appNames = {};
+
 client.on('ready', () => {
 	console.log('Ready to spy on ' + client.users.size + ' users, in ' + client.channels.size + ' channels of ' + client.guilds.size + ' servers as ' + client.user.tag + '.');
 });
@@ -21,8 +24,6 @@ client.once('ready', () => {
 		try {
 			json = JSON.parse(body);
 		} catch(err) {};
-
-		var appNames = {};
 
 		if (!json || !json.applist || !json.applist.apps || json.applist.apps.length < 1) {
 			console.log('Malformed Steam API response\n\n' + body);
@@ -40,100 +41,143 @@ client.once('ready', () => {
 		}
 		if (!appNames) return console.log('Failed to get apps from Steam API and couldnt get apps.json');
 
-		const ws = new WebSocket('ws://localhost:8181');
-
-		const child = childProcess.spawn('dotnet', [ config.SteamWebPipes ]);
-		child.on('exit', (code, signal) => {
-			console.log('Exited with code ' + code + ' - Signal: ' + signal);
-		});
-
-		ws.onopen = () => {
-			console.log('Successfully connected to WebSocket');
-		};
-
-		ws.onclose = () => {
-			console.log('Disconnected from WebSocket');
-		};
-
-		ws.onerror = (error) => {
-			console.error(error);
-		};
-
-		ws.onmessage = (e) => {
-			if (e.type !== 'message') {
-				console.log(e);
-				// ???
-				return;
-			}
-
-			var json = undefined;
-			try {
-				json = JSON.parse(e.data);
-			} catch (err) {};
-			if (!json) return console.log(e.data);
-
-			if (json.Type == 'LogOn') {
-				console.log('Successfully connected to SteamWebPipes');
-				return;
-			}
-
-			if (json.Type == 'LogOff') {
-				console.log('Disconnected from SteamWebPipes');
-				return;
-			}
-
-			if (client.status !== 0) return;
-
-			if (json.Type === 'UsersOnline') {
-				client.user.setPresence({ activity: { name: 'Steam app updates', type: 'WATCHING' }});
-				return;
-			}
-
-			if (json.Type === 'Changelist') {
-				const embed = new Discord.MessageEmbed();
-				embed.setTimestamp();
-				embed.setDescription('Change number: ' + json.ChangeNumber);
-				embed.setColor(0);
-				embed.setURL('https://steamdb.info/changelist/' + json.ChangeNumber + '/');
-
-				var apps = Object.keys(json.Apps); // We only care about app changes, not package changes
-				if (!apps || apps.length < 1) return;
-
-				embed.setTitle(apps.length + ' app ' + (apps.length === 1 ? 'change' : 'changes'));
-
-				var mention = false;
-				for (let i = 0; i < apps.length; i++) {
-					if (config.watchedApps.includes(apps[i])) mention = true;
-
-					if (i >= 25) {
-						console.log('Could not add changes for ' + apps[i] + ' because the embed would be too long');
-					} else {
-						var appName = json.Apps[apps[i]];
-						if (appName.includes('Unknown App ' + apps[i])) {
-							if (appNames[apps[i]]) {
-								appName = appNames[apps[i]];
-							}
-						}
-
-						embed.addField(apps[i] + ' - ' + appName, '[SteamDB](https://steamdb.info/app/' + apps[i] + '/history/) - [Steam Store](https://store.steampowered.com/app/' + apps[i] + '/)');
-					}
-				}
-
-				if (mention) embed.setColor([ 0, 255, 0 ]); // Bright green for watched apps
-
-				if (!client.channels.get(config.channel)) return;
-
-				if (mention) client.channels.get(config.channel).send(client.channels.get(config.channel).guild.roles.get(config.mentionRole).toString(), {embed: embed}).catch((e) => console.error(e));
-				else client.channels.get(config.channel).send({embed: embed}).catch((e) => console.error(e));
-				return;
-			}
-
-			console.log(e);
-		};
+		startChildProcess();
 	});
 });
 
 client.on('warn', (warn) => console.warn(warn));
-client.on('error', (error) => console.error(error));
+client.on('error', (error) => {
+	// There should never be an error unless it is a connection error
+	restartWebSocket();
 
-client.login(config.botToken);
+	console.error(error);
+});
+
+client.login(config.botToken).catch((e) => console.error(e));
+
+function startChildProcess() {
+	const child = childProcess.spawn('dotnet', [ config.SteamWebPipes ]);
+	child.on('exit', (code, signal) => {
+		console.log('Exited with code ' + code + ' - Signal: ' + signal);
+
+		setTimeout(startChildProcess, 5000);
+	});
+
+	var once = false;
+
+	child.stdout.on('data', (data) => {
+		if (once === false) {
+			once = true;
+			setTimeout(startWebSocketConnection, 5000);
+		}
+	});
+}
+
+function restartWebSocket() {
+	if (ws.readyState === 1) {
+		ws.close();
+
+		ws.onclose = () => {
+			console.log('Disconnected from WebSocket');
+
+			ws = undefined;
+
+			setTimeout(startWebSocketConnection, 5000);
+		};
+	} else if (ws.readyState === 2 || ws.readyState === 3) {
+		function isConnectionClosed() {
+			if (ws.readyState === 3) {
+				setTimeout(startWebSocketConnection, 5000);
+			} else {
+				setTimeout(isConnectionClosed, 1000);
+			}
+		}
+		setTimeout(isConnectionClosed, 1000);
+	}
+}
+
+function startWebSocketConnection() {
+	ws = new WebSocket('ws://localhost:8181');
+
+	ws.onopen = () => {
+		console.log('Successfully connected to WebSocket');
+	};
+
+	ws.onerror = (error) => {
+		console.error(error);
+	};
+
+	ws.onmessage = (e) => {
+		if (e.type !== 'message') {
+			console.log(e);
+			// ???
+			return;
+		}
+
+		var json = undefined;
+		try {
+			json = JSON.parse(e.data);
+		} catch (err) {};
+		if (!json) return console.log(e.data);
+
+		if (json.Type == 'LogOn') {
+			console.log('Successfully connected to SteamWebPipes');
+			return;
+		}
+
+		if (json.Type == 'LogOff') {
+			console.log('Disconnected from SteamWebPipes');
+			return;
+		}
+
+		if (client.status !== 0) return;
+
+		if (json.Type === 'UsersOnline') {
+			client.user.setPresence({ activity: { name: 'Steam app updates', type: 'WATCHING' }}).catch(() => {});
+			return;
+		}
+
+		if (json.Type === 'Changelist') {
+			const embed = new Discord.MessageEmbed();
+			embed.setTimestamp();
+			embed.setDescription('Change number: ' + json.ChangeNumber);
+			embed.setColor(0);
+			embed.setURL('https://steamdb.info/changelist/' + json.ChangeNumber + '/');
+
+			var apps = Object.keys(json.Apps); // We only care about app changes, not package changes
+			if (!apps || apps.length < 1) return;
+
+			embed.setTitle(apps.length + ' app ' + (apps.length === 1 ? 'change' : 'changes'));
+
+			var mention = false;
+			for (let i = 0; i < apps.length; i++) {
+				if (config.watchedApps.includes(apps[i])) mention = true;
+
+				if (i >= 24) {
+					embed.addField('And many more!', 'There are a total of ' + apps.length + ' app changes!');
+
+					console.log('Could not add changes for ' + apps[i] + ' because the embed would be too long');
+				} else {
+					var appName = json.Apps[apps[i]];
+					if (appName.includes('Unknown App ' + apps[i])) {
+						if (appNames[apps[i]]) {
+							appName = appNames[apps[i]];
+						}
+					}
+
+					embed.addField(apps[i] + ' - ' + appName, '[SteamDB](https://steamdb.info/app/' + apps[i] + '/history/) - [Steam Store](https://store.steampowered.com/app/' + apps[i] + '/)');
+				}
+			}
+
+			if (mention) embed.setColor([ 0, 255, 0 ]); // Bright green for watched apps
+
+			if (!client.channels.get(config.channel)) return;
+
+			if (mention) client.channels.get(config.channel).send(client.channels.get(config.channel).guild.roles.get(config.mentionRole).toString(), {embed: embed}).catch(() => {});
+			else client.channels.get(config.channel).send({embed: embed}).catch(() => {});
+			return;
+		}
+
+		console.log(e);
+	};
+}
