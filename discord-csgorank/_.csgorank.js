a0bc81f8aee5367b32a8bd39c77c6687539ac859
Felix <DukeBruno123@gmail.com>
2018-09-18 18:15:34 +0200

    Comments

--- discord-csgorank/_.csgorank.js
+++ discord-csgorank/_.csgorank.js
@@ -10,11 +10,13 @@ const discordClient = new Discord.Client();
 const steamClient = new SteamUser();
 const csgo = new GlobalOffensive(steamClient);
 
+// Setup the login settings for Steam
 var logonSettings = {
 	accountName: config.steam.account.username,
 	password: config.steam.account.password
 };
 
+// Generate a 2FA code if we have a sharedSecret provided
 if (config.steam.account.sharedSecret && config.steam.account.sharedSecret.length > 5) {
 	logonSettings.twoFactorCode = SteamTotp.getAuthCode(config.steam.account.sharedSecret);
 	console.log('Generated steam guard code: ' + logonSettings.twoFactorCode);
@@ -22,19 +24,25 @@ if (config.steam.account.sharedSecret && config.steam.account.sharedSecret.lengt
 	console.log('No shared secret provided');
 }
 
+// Usual logging
 discordClient.on('ready', () => {
 	console.log('Ready to spy on ' + discordClient.users.size + ' users, in ' + discordClient.channels.size + ' channels of ' + discordClient.guilds.size + ' servers as ' + discordClient.user.tag + '.');
 });
 
+// Create a map so we know which user is related to which Steam account
 var onGoing = new Map();
 
+// Listen to messages
 discordClient.on('message', async (msg) => {
+	// Command system
 	if (!msg.guild || msg.content.indexOf(config.prefix) !== 0) return;
 
 	const args = msg.content.split(/ +/g);
 	const command = args.shift().slice(config.prefix.length).toLowerCase();
 
+	// "rank" command
 	if (command === 'rank') {
+		// If we are inside the "onGoing" map check if it has been 10 or more minutes since the initial request
 		if (onGoing.has(msg.author.id)) {
 			var value = onGoing.get(msg.author.id);
 			if ((new Date().getTime() - value.date.getTime()) >= (10 * 60 * 1000)) { // 10 minute timeout
@@ -45,11 +53,14 @@ discordClient.on('message', async (msg) => {
 			}
 		}
 
+		// We need an argument (eg: Steam Profile) in order for this to work
 		if (!args[0]) {
 			msg.channel.send('Please use `' + config.prefix + 'rank <Your Steam Profile Link>` to get yourself a rank');
 			return;
 		}
 
+		// Parse Steam ID out of given text
+		// Copy pasted from my Steam Ban Notifier: https://github.com/BeepFelix/Discord-SteamBan-Notifier/blob/master/modules/functions.js#L30
 		new Promise((resolve, reject) => {
 			var check = args[0];
 			check = check.replace('http://www.steamcommunity.com/profiles/', '');
@@ -128,6 +139,7 @@ discordClient.on('message', async (msg) => {
 				resolve(json.response.steamid);
 			});
 		}).then((steamid) => {
+			// We got a steam ID, request the user to add us
 			msg.channel.send({ embed: {
 				title: 'Please add me on Steam',
 				url: 'https://steamcommunity.com/profiles/' + steamClient.steamID.getSteamID64(),
@@ -136,6 +148,7 @@ discordClient.on('message', async (msg) => {
 				onGoing.set(msg.author.id, { date: new Date(), steamid: steamid, channel: m.channel.id, message: m.id });
 			});
 		}).catch((err) => {
+			// Print error
 			if (typeof err === 'string') {
 				msg.channel.send({ embed: {
 					title: 'Error',
@@ -167,14 +180,23 @@ steamClient.on('loggedOn', (details) => {
 	steamClient.setPersona(SteamUser.Steam.EPersonaState.Online);
 	steamClient.gamesPlayed([ 730 ]) // Start CSGO
 
+	// If we cannot log into discord terminate the process
 	discordClient.login(config.botToken).catch((e) => {
 		console.error(e);
 		process.exit(1);
 	});
 });
 
+// Terminate the process if we have an error during login
+discordClient.on('error', (e) => {
+	console.error(e);
+	process.exit(1);
+});
+
+// Check for relationship changes
 steamClient.on('friendRelationship', async (sid, relationship) => {
-	if (relationship === SteamUser.EFriendRelationship.Friend) {
+	if (relationship === SteamUser.EFriendRelationship.Friend) { // If the relationship changes to "Friend" it means we are now friends
+		// Find the user in our "onGoing" map
 		var found = undefined;
 		onGoing.forEach((value, key) => {
 			if (value.steamid === sid.getSteamID64()) {
@@ -187,14 +209,17 @@ steamClient.on('friendRelationship', async (sid, relationship) => {
 			return;
 		}
 
-		user = discordClient.users.get(found.discord);
+		// Get user and channel
+		var user = discordClient.users.get(found.discord);
 		var channel = discordClient.channels.get(found.channel);
-		if (!channel) return; // The channel has been deleted while processing the request
+		if (!channel || !user) return; // No more channel or user
 
+		// Fetch our own message from earlier so we can edit it
 		var m = await channel.messages.fetch(found.message).catch(() => {});
 
+		// Get CSGO user profile (Since recent GC changes you can now request the player rank without the user having to have CSGO running)
 		csgo.requestPlayersProfile(found.steam, async (profile) => {
-			steamClient.removeFriend(sid);
+			steamClient.removeFriend(sid); // Remove the user from our friend list he is no longer needed
 
 			if (!channel) return; // We couldn't find the channel the message was written in. Something went wrong
 
@@ -210,9 +235,11 @@ steamClient.on('friendRelationship', async (sid, relationship) => {
 			// Remove all rank roles incase the user has any
 			await member.roles.remove(config.ranks).catch(() => {});
 
+			// Add the one specific role for the users rank
 			member.roles.add(config.ranks[profile.ranking.rankId]).then(() => {
 				if (!m || m.deleted) return;
 
+				// Edit the message with success if we were able to fetch it
 				m.edit({ embed: {
 					title: 'You have been assigned ' + member.guild.roles.get(config.ranks[profile.ranking.rankId]).name,
 					color: Discord.Util.resolveColor('#00ff00')
@@ -220,6 +247,7 @@ steamClient.on('friendRelationship', async (sid, relationship) => {
 			}).catch((e) => {
 				if (!m || m.deleted) return;
 
+				// Edit the message with the error if we were able to fetch it
 				m.edit({ embed: {
 					title: 'Failed to assign a role',
 					description: e.message || e,
@@ -227,7 +255,8 @@ steamClient.on('friendRelationship', async (sid, relationship) => {
 				}});
 			});
 		});
-	} else if (relationship === SteamUser.EFriendRelationship.RequestRecipient) {
+	} else if (relationship === SteamUser.EFriendRelationship.RequestRecipient) { // If the relationship changes to "outstanding" it means we got an incoming friend request
+		// Find the user in our "onGoing" map
 		var found = false;
 		onGoing.forEach((value) => {
 			if (value.steamid === sid.getSteamID64()) {
@@ -240,13 +269,9 @@ steamClient.on('friendRelationship', async (sid, relationship) => {
 			return;
 		}
 
+		// Accept friend request. "friendRelationship" will automatically trigger again
 		steamClient.addFriend(sid);
 	}
-});
-
-discordClient.on('error', (e) => {
-	console.error(e);
-	process.exit(1);
 });
 
 process.on('uncaughtException', (e) => console.error(e));
